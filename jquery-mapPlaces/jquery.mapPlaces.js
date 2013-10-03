d5b1afb4d4d0bdaab2a0d01f26fd2c8749b87349
Sean Metzgar <sean.metzgar@gmail.com>
2013-10-03 14:21:26 -0400

    Placing points on map

--- jquery-mapPlaces/jquery.mapPlaces.js
+++ jquery-mapPlaces/jquery.mapPlaces.js
@@ -21,9 +21,34 @@
         internal.defaults = {
             loaderText: "Loading...",
             apiKey: "AIzaSyCaN2XlwZVROBpQB42yk9WNVwBtLJZgkMc",
-            mapZoom: 12,
+            mapZoom: 13,
             mapLat: 40.0306,
-            mapLong: -75.2328
+            mapLong: -75.2328,
+            icons: {},
+            radiusValues: [
+                {
+                    name: "1/4 Mile",
+                    meters: 402
+                },
+                {
+                    name: "1/2 Mile",
+                    meters: 805
+                },
+                {
+                    name: "1 Mile",
+                    meters: 1609
+                },
+                {
+                    name: "2 Miles",
+                    meters: 3219
+                },
+            ],
+            categories: [
+                {
+                    name: "Services",
+                    types: []
+                }
+            ]
         };
         /** Elements / jQuery Objects **/
         internal.$element = $(element);
@@ -31,18 +56,21 @@
         internal.$container = null;
         internal.$loader = null;
         internal.$foundation = null;
+        /** Internal Variables **/
         internal.mapCenter = null;
         internal.mapProp = {};
         internal.map = null;
+        internal.placesService = null;
         internal.mapAPI = (typeof window.google !== "undefined" && typeof window.google.maps !== "undefined") ? window.google.maps : false;
+        internal.mapMarkers = [];
+        internal.mapInfoWindow = null;
         internal.classes = {
             containerClass: "mapPlacesContainer",
             loaderClass: "mapPlacesLoader",
             foundationClass: "mapPlacesFoundation"
         };
         /** Internal Settings Declaration **/
-        internal.settings = {
-        };
+        internal.settings = {};
         /** Internal Methods **/
         internal.methods = {
             /** Constructor Method **/
@@ -55,6 +83,7 @@
                     $.getScript('https://www.google.com/jsapi', function() {
                         google.load('maps', 3, { other_params: 'libraries=places&sensor=false', callback: function() {
                             internal.mapAPI = window.google.maps;
+                            internal.mapInfoWindow = new internal.mapAPI.InfoWindow();
                             internal.methods.buildMap();
                         }});
                     });
@@ -69,16 +98,59 @@
                 };
                 internal.map = new internal.mapAPI.Map(internal.$foundation.get(0), internal.mapProp);
                 internal.placesService = new internal.mapAPI.places.PlacesService(internal.map);
+                internal.methods.nearbySearch(['store','church']);
             },
-            nearbySearch: function() {
-                internal.placesService.nearbySearch({}, internal.methods.nearbySearchCallback);
+            nearbySearch: function(types, radius) {
+                var request = {},
+                    radius = (typeof radius === "number") ? radius : internal.settings.radiusValues[internal.settings.radiusValues.length - 1].meters;
+
+                if (Array.isArray(types)) {
+                    request = {
+                        location: internal.mapCenter,
+                        radius: radius,
+                        types: types
+                    };
+                    internal.placesService.nearbySearch(request, internal.methods.nearbySearchCallback);
+                }
             },
-            
+            nearbySearchCallback: function (results, status) {
+                var place = null,
+                    i = 0;
+                if (status == internal.mapAPI.places.PlacesServiceStatus.OK) {
+                    for (i = 0; i < results.length; i=i+1) {
+                        place = results[i];
+                        internal.methods.createMarker(place);
+                    }
+                }
+            },
+            createMarker: function (place) {
+                var location = null,
+                    marker = null;
+                
+                if (typeof place === "object") {
+                    location = place.geometry.location;
+                    marker = new internal.mapAPI.Marker({
+                        map: internal.map,
+                        position: location
+                    });
+
+                    internal.mapAPI.event.addListener(marker, 'click', function() {
+                        console.log(place);
+                        internal.mapInfoWindow.setContent(place.name);
+                        internal.mapInfoWindow.open(internal.map, this);
+                    });
+                } 
+            }         
         };
         /** Initialization Method **/
         plugin.init = function () {
             internal.settings = $.extend({}, internal.defaults, options);
             internal.methods.buildFramework();
+            if(!Array.isArray) {
+                Array.isArray = function (vArg) {
+                    return Object.prototype.toString.call(vArg) === "[object Array]";
+                };
+            }
         };
         plugin.init();
     };
